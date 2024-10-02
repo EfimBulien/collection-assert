1af4944dab85d10018ff88912545689186596fe1
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-10-02 17:14:52 +0300

    second commit

--- CollectionMathTests/MathCollectionTests.cs
+++ CollectionMathTests/MathCollectionTests.cs
@@ -23,18 +23,19 @@ namespace CollectionMathTests
         public void FindRoots_TwoRoots_DiscriminantGreaterThanZero()
         {
             int expected = 2;
+            double delta = 0.001;
+            double expected1 = 2;
+            double expected2 = 1;
 
             double a = _testRootsCollection[0][0];
             double b = _testRootsCollection[0][1];
             double c = _testRootsCollection[0][2];
 
             double[] actual = MathCollection.FindRoots(a, b, c);
-            double expected1 = 2;
-            double expected2 = 1;
             
             Assert.AreEqual(expected, actual.Length, $"Ожидается {expected} корня для a={a}, b={b}, c={c}");
-            Assert.AreEqual(expected1, actual[0], 0.001, $"Ожидаемый корень 1 для a={a}, b={b}, c={c}: {expected1}");
-            Assert.AreEqual(expected2, actual[1], 0.001, $"Ожидаемый корень 2 для a={a}, b={b}, c={c}: {expected2}");
+            Assert.AreEqual(expected1, actual[0], delta, $"Ожидаемый корень 1 для a={a}, b={b}, c={c}: {expected1}");
+            Assert.AreEqual(expected2, actual[1], delta, $"Ожидаемый корень 2 для a={a}, b={b}, c={c}: {expected2}");
         }
 
         // Тест для случая D = 0 (один корень)
@@ -42,6 +43,7 @@ namespace CollectionMathTests
         public void FindRoots_OneRoot_DiscriminantEqualToZero()
         {
             int expected = 1;
+            double delta = 0.001;
             double expected1 = -1;
 
             double a = _testRootsCollection[1][0];
@@ -51,7 +53,7 @@ namespace CollectionMathTests
             double[] actual = MathCollection.FindRoots(a, b, c);
             
             Assert.AreEqual(expected, actual.Length, $"Ожидается {expected} корень для a={a}, b={b}, c={c}");
-            Assert.AreEqual(expected1, actual[0], 0.001, $"Ожидаемый корень для a={a}, b={b}, c={c}: {expected1}");
+            Assert.AreEqual(expected1, actual[0], delta, $"Ожидаемый корень для a={a}, b={b}, c={c}: {expected1}");
         }
 
         // Тест для случая D < 0 (корней нет)

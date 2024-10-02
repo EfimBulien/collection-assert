2634f987d50ef653a0f08c2da2021df1af92b18f
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-10-02 17:10:22 +0300

    first commit

---
+++ CollectionMath/MathCollection.cs
@@ -0,0 +1,37 @@
+﻿namespace CollectionMath
+{
+    public class MathCollection
+    {
+        // Метод для нахождение корней через дискриминант
+        public static double[] FindRoots(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                // Нет корней
+                return [];
+            }
+            else if (discriminant == 0)
+            {
+                // Один корень
+                double root = -b / (2 * a);
+                return [root];
+            }
+            else
+            {
+                // Два корня
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                double root1 = (-b + sqrtDiscriminant) / (2 * a);
+                double root2 = (-b - sqrtDiscriminant) / (2 * a);
+                return [root1, root2];
+            }
+        }
+
+        // Метод для вычисления процента от числа
+        public static double CalculatePercentage(double number, double percentage)
+        {
+            return number * (percentage / 100);
+        }
+    }
+}
